bc9ddc3bdbad36e304612a0dfab91c2552f8c9d2
zyt <2503537090@qq.com>
2019-08-17 22:53:50 +0800

    can reply vehicle register data

--- data_parse.py
+++ data_parse.py
@@ -21,27 +21,28 @@ class DataParse:
                     data = binfile.read()
                 if not data:
                     break
-                output = ""
-                # data = self.rfile.read(1024)
-                for each in data:
-                    output = output + "{:0>2x}".format(each) + " "
-                process_message(data)
-                print("output: ", output)
+                print("原始数据：")
+                output = print_0x(data)
+
+                reply = self.process_message(data)
+                if reply == -1:
+                    print("出错！")
                 with open("binlog.txt", "wb+") as f:
                     f.write(data)
                 with open("strlog.txt", "w+") as e:
                     e.write(output)
                 # print("data: " + data.hex())
                 # print(type(data))
+                print("vehicle_register_reply:")
+                print_0x(reply)
                 input()
                 print('receive from (%r):%r' % (self.client_address, output))
-                self.request.sendall(data.upper())
+                self.request.sendall(reply)
             except:
                 traceback.print_exc()
                 break
 
     # 判断所收到数据开头是2323
-
 
     # 解析时间戳
     @staticmethod
@@ -62,96 +63,121 @@ class DataParse:
     def vehicle_position_parse(self):
         pass
 
+    @staticmethod
+    def process_message(data):
+        # print("原始数据: ", binascii.b2a_hex(data))
+        if data[0] != 0x23 or data[1] != 0x23:
+            print("首字节非2323!")
+            return -1
 
-def process_message(data):
-    print("data: ", binascii.b2a_hex(data))
-    if data[0] != 0x23 or data[1] != 0x23:
-        print("首字节非2323!")
-        return data
+        # 提取数据单元
+        data_unit = extract_data_unit(data)
 
-    # 处理标识位
-    if data[2] == 0x01:
-        print("车辆登入")
+        # 比较校验码
+        [checkres, check_code] = check_sum(data)
+        if checkres != 0x00:
+            print("校验失败！")
+            return -1
 
-        # 应答
+        # 提取车辆VIN码
+        VIN = data[4:20]
+        print("VIN: ", VIN)
+        print_0x(VIN)
 
-        # to do
-    elif data[2] == 0x02:
-        print("实时信息上报数据")
-        # to do
-    elif data[2] == 0x03:
-        print("补发信息上报")
-        # to do
-    elif data[2] == 0x04:
-        print("车辆登出")
-        # to do
-    elif data[2] == 0x05:
-        print("平台登入")
-        # to do
-    elif data[2] == 0x06:
-        print("平台登出")
-        # to do
-    elif data[2] == 0x03:
-        print("车辆登出")
-        # to do
-    elif data[2] <= 0x08 & data[2] >= 0x07:
-        print("终端数据预留（上行）")
-        # to do
-    elif data[2] <= 0x7F & data[2] >= 0x09:
-        print("上行数据系统预留")
-        # to do
-    # elif data[2] <= 0x82 & data[2] >= 0x80:
-    #     print("终端数据预留（下行）")
-    #     # to do
-    # elif data[2] <= 0x82 & data[2] >= 0x80:
-    #     print("下行数据系统预留")
-    #     # to do
-    elif data[2] <= 0xFE & data[2] >= 0xC0:
-        print("平台自定义数据")
-        # to do
-    else:
-        print("命令标识无法识别!")
-        return -1
-
-    # 处理应答位
-    if data[2] == 0xFE:
-        print("此包为命令包,需要应答！")
-        # to do
-    else:
-        print("此包不需要应答")
-        # to do
-
-    # 提取车辆VIN码
-    VIN = data[4:20]
-    print("VIN: ", VIN)
-
-    # 提取加密方式
-    if data[21] == 0x01:
-        print("无加密")
-        # to do
-    else:
-        print("有加密")
-        # to do
+        # 提取加密方式
+        encode_type = data[21]
+        if encode_type == 0x01:
+            print("无加密")
+            # to do
 
 
+        # 处理标识位
+        if data[2] == 0x01:
+            print("车辆登入!")
+            # 处理应答位
+            if data[3] == 0xFE:
+                print("此包为命令包,需要应答！")
+                reply = get_vehicle_register_reply(data)
+                return reply
+                # to do
+            else:
+                print("此包不需要应答")
+                return 0
+                # to do
 
-    # 比较校验码
-    [checkres, check_code] = check_sum(data)
-    if checkres != 0x00:
-        print("校验失败！")
-        return -1
+            # 应答
+            # to do
+        elif data[2] == 0x02:
+            print("实时信息上报数据")
+            # to do
+        elif data[2] == 0x03:
+            print("补发信息上报")
+            # to do
+        elif data[2] == 0x04:
+            print("车辆登出")
+            # to do
+        elif data[2] == 0x05:
+            print("平台登入")
+            # to do
+        elif data[2] == 0x06:
+            print("平台登出")
+            # to do
+        elif data[2] == 0x03:
+            print("车辆登出")
+            # to do
+        elif data[2] <= 0x08 & data[2] >= 0x07:
+            print("终端数据预留（上行）")
+            # to do
+        elif data[2] <= 0x7F & data[2] >= 0x09:
+            print("上行数据系统预留")
+            # to do
+        # elif data[2] <= 0x82 & data[2] >= 0x80:
+        #     print("终端数据预留（下行）")
+        #     # to do
+        # elif data[2] <= 0x82 & data[2] >= 0x80:
+        #     print("下行数据系统预留")
+        #     # to do
+        elif data[2] <= 0xFE & data[2] >= 0xC0:
+            print("平台自定义数据")
+            # to do
+        else:
+            print("命令标识无法识别!")
+            return -1
 
-    data_unit = extract_data_unit(data)
-    time_now = current_time()
-    print("time_now: ", time_now)
-    input()
+
 
 
 # 应答数据(应答位替换, 时间替换, 校验位替换)
-def reply_data(data, time_now, check_code):
+def get_vehicle_register_reply(data):
+    time_now = current_time()
+    print("替换前的数据： ", data)
+    print("替换前的数据类型： ", type(data))
+    data = bytearray(data)
+    print("before:")
+    print_0x(data)
     data[3] = 0x02
-    data[-1] = hex(check_code)
+    data[22] = time_now[0] % 2000
+    data[23] = time_now[1]
+    data[24] = time_now[2]
+    data[25] = time_now[3]
+    data[26] = time_now[4]
+    data[27] = time_now[5]
+    [_, check_code] = check_sum(data)
+    data[-1] = check_code
+    vehicle_register_reply = bytes(data)
+    print("after:")
+    print_0x(vehicle_register_reply)
+    return vehicle_register_reply
 
+
+# 转码显示
+def print_0x(data, show_result=True):
+    output = ""
+    for each in data:
+        output = output + "{:0>2x}".format(each) + " "
+    if show_result:
+        print(output)
+    return output
 
 
 # 提取数据单元
@@ -164,7 +190,8 @@ def extract_data_unit(data):
     # 提取数据单元
     end_position = 24 + data_unit_length
     data_unit = data[24:end_position]
-    print("数据单元：", data_unit)
+    print("数据单元：")
+    print_0x(data_unit)
     return data_unit
 
 
@@ -173,8 +200,7 @@ def current_time():
     # 获取当前时间 格式：%Y.%m.%d %H:%M:%S
     ori_time = time.localtime(time.time())
     time_now = [ori_time.tm_year, ori_time.tm_mon, ori_time.tm_mday, ori_time.tm_hour,
-         ori_time.tm_min, ori_time.tm_sec]
-
+                ori_time.tm_min, ori_time.tm_sec]
     return time_now
 
 
@@ -192,14 +218,9 @@ def check_sum(data):
     checksum = 0
     for i in range(0, size - 1):
         checksum = checksum ^ data[i]
-    print(output)
     print("checksum: " + "{:0>2x}".format(checksum))
     print("checkres: " + "{:0>2x}".format(checkres))
-    if checkres == 0x00:
-        return [checkres, checksum]
-    else:
-        return [checkres, checksum]
-
+    return [checkres, checksum]
 
 if __name__ == '__main__':
     # HOST, PORT = 'localhost', 8105
